50f600967f4e92e80a5690e04e612d181f8968a6
Will <matt@cdjm.co.uk>
2016-02-05 23:40:06 +0000

    Upated index and added contact    .php

--- index.php
+++ index.php
@@ -8,12 +8,19 @@
     <body>
         <div>
             <!-- PUT HEADINGS IN HERE -->
-            
+            <h1>"Welcome to Foodle"</h1>
+	    <h3>"Your Campus Waiter"</h3> 
 
         </div>
 
 
         <!-- FOOTER -->
+	<?php
+	echo "Copyright notice<br>"
+	echo "Foodle© - Created by the Foodle corp. 2016<br>"
+	echo "Contributors: Jamie, Will, Matt, with thanks to our avid fan Mike<br>"
+	echo "Shared under some all inclusive open license...."
+	?>
 
     </body>
 </html>
