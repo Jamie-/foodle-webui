6fb594736ab5e055a7663230dda1389c784eee15
jamie- <github@jami.org.uk>
2016-02-05 22:44:32 +0000

    added css link

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php phpinfo();
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Foodle.com</title>
+        <link rel="stylesheet" href="css/default.css" />
+    </head>
+
+    <body>
+        
+    </body>
+</html>
